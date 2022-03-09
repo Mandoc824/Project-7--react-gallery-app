d862da68d8e6622e5d66c90afdc625679b940f84
Armando Carera <armanxzmc@hotmail.com>
2022-03-09 02:17:53 -0500

    adding finishing touches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
+//components
+import SearchForm from "./components/SearchForm";
+import Nav from "./components/Nav";
+import PhotoContainer from "./components/PhotoContainer";
+
+//api key
+import apiKey from "./config";
+const key = apiKey;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <SearchForm />
+        <Nav />
+        <Routes>
+          <Route
+            exact
+            path="/:search"
+            element={<PhotoContainer apiKey={key} />}
+          />
+          <Route path="/" element={<Navigate replace to="/search" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/PhotoContainer.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import Photo from "./Photo";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import NotFound from "./NotFound";
+const PhotoContainer = (props) => {
+  const [results, setResults] = useState([]);
+
+  let { search } = useParams();
+
+  useEffect(() => {
+    const FetchData = (query) => {
+      axios
+        .get(
+          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${props.apiKey}&text=${query}&media=photos&extras=url_m&per_page=24&format=json&nojsoncallback=1`
+        )
+        .then((response) => setResults(response.data.photos.photo))
+        .catch((err) => console.error("Error fetching and parsing data", err));
+    };
+    FetchData(search);
+  }, [search, props.apiKey]);
+
+  return (
+    <div className="photo-container">
+      {results.length !== 0 ? (
+        <>
+          <h2>Results For: {search}</h2>
+          <ul>
+            {results.map((picture) => (
+              <Photo
+                key={picture.id}
+                url={picture.url_m}
+                desc={picture.title}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <NotFound />
+      )}
+    </div>
+  );
+};
+
+export default PhotoContainer;
